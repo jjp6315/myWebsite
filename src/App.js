9fe4de78c48b95c3ef73b6f51e25ea69c3e2e925
Ji Woong Park <johnpark0528@gmail.com>
2023-07-24 00:02:11 -0400

    moving everything over to my-app

--- src/App.js
+++ src/App.js
@@ -3,26 +3,48 @@ import "./App.css";
 import Box from "./Components/Box";
 import Circle from "./Components/Circle";
 import styled from "styled-components";
+import Iphone from "./Components/Iphone";
+import Airpods from "./Components/Airpods";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import About from "./Components/About";
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
-    <Wrapper>
-      <Canvas className="canvas">
-        <OrbitControls enableZoom={false} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
-        <Box />
-      </Canvas>
-      <Canvas className="canvas">
-        <OrbitControls enableZoom={false} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
-        <Circle />
-      </Canvas>
-    </Wrapper>
+    <div id="Main">
+      <Header />
+      <Home name={"Ji Woong John Park"} title={"Computer Science Student"} />
+      <About />
+      <Wrapper>
+        <Canvas className="canvas">
+          <OrbitControls enableZoom={false} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} intensity={1} />
+          <Box />
+        </Canvas>
+        <Canvas className="canvas">
+          <OrbitControls enableZoom={false} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} intensity={1} />
+          <Circle />
+        </Canvas>
+        <Canvas className="canvas">
+          <OrbitControls enableZoom={false} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} intensity={1} />
+          <Iphone />
+        </Canvas>
+        <Canvas className="canvas">
+          <OrbitControls enableZoom={true} />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} intensity={1} />
+          <Airpods />
+        </Canvas>
+      </Wrapper>
+    </div>
   );
 }
 
