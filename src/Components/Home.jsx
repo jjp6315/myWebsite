408c705d99edc9c0aa154317f8edb3fa06f89700
Ji Woong Park <johnpark0528@gmail.com>
2023-07-26 23:39:45 -0400

    hosted on firebase

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,17 +10,6 @@ import arrowSvg from "../images/down-arrow.svg";
 import PropTypes from "prop-types";
 import { Cursor, useTypewriter, Typewriter } from "react-simple-typewriter";
 
-/**
- * Home background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that
- * represents what you see in that image.
- *
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
 import image from "../images/seoulNight.jpg";
 import portfolio from "../images/profile.jpg";
 
@@ -186,7 +175,6 @@ const Home = ({ name }) => {
             alt={imageAltText}
           />
         </a>
-        {/* <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} /> */}
       </div>
       <div
         style={{
@@ -212,15 +200,6 @@ const Home = ({ name }) => {
       </div>
     </section>
   );
-};
-
-// Home.defaultProps = {
-//   name: "",
-//   title: "",
-// };
-
-Home.propTypes = {
-  name: PropTypes.string.isRequired,
 };
 
 export default Home;
